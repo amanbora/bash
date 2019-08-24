387878554ac0e32b5fefeeb1f227d032399f1126
aman <aman@github.com>
2019-08-25 02:26:00 +0530

    check xxx/(xxx)-xxx-xxxx format interview bit

--- validPhnNum.sh
+++ validPhnNum.sh
@@ -1,6 +1,1 @@
-while IFS = read -r line; do
-  if[[$line =~ ^([[:digit:]]{3}:){1}[[:digit:]]{4}$ ]]
-    then echo $line
-  elif[[$line == ^([[(digit)]]{3}:){1}[[:digit:]]{4} ]]
-    then echo $line
-  fi
+grep -P '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' input
